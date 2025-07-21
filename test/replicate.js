ec7a286694f020f079774c79c7e119ac76fe3ee4
Mathias Buus <mathiasbuus@gmail.com>
2025-07-21 19:43:57 +0200

    more close tests (#60)
    * solo test
    
    * unsolo
    
    ---------
    
    Co-authored-by: Christophe Diederichs <chm-diederichs@protonmail.com>

--- test/replicate.js
+++ test/replicate.js
@@ -136,3 +136,35 @@ test.bee('requests are cleared on close', async function ({ create }, t) {
     await clone.close()
   })
 })
+
+test.bee('requests are cleared on close from unclosed snap', async function ({ create }, t) {
+  const db = await create()
+
+  await db.insert('@db/members', { id: 'someone', age: 40 })
+  await db.flush()
+
+  const clone = await create({ key: db.core.key, autoUpdate: true })
+
+  t.alike(clone.core.key, db.core.key)
+
+  const unreplicate = replicate(t, clone, db)
+
+  await new Promise(resolve => clone.core.once('append', resolve))
+
+  await unreplicate()
+
+  const snap = clone.snapshot()
+  const members = snap.get('@db/members', { id: 'someone' })
+  const cancelled = t.exception(members, /REQUEST_CANCELLED/)
+
+  await new Promise(resolve => setTimeout(resolve, 10))
+
+  await clone.close()
+
+  await cancelled
+
+  t.teardown(async () => {
+    await db.close()
+    await clone.close()
+  })
+})

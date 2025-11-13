71c1308f198961bf41a20d4b53e07890b9ab6ee5
Dominic Cassidy <dominic@cassidy.casa>
2025-11-13 14:43:56 +0200

    generated fixtures

--- test/update.js
+++ test/update.js
@@ -3,22 +3,22 @@ const { test } = require('./helpers')
 test('update nested', async function ({ create, bee }, t) {
   const db = await create({ fixture: 7 })
   const builder = require('./fixtures/generated/7/hyperschema')
-  const { Male } = builder.getEnum('@db/gender')
+  const { NotSpecified } = builder.getEnum('@db/gender')
 
   await db.insert('@db/members', {
     name: 'John',
-    gender: Male
+    gender: NotSpecified
   })
   await db.flush()
 
   {
     const john = await db.get('@db/members', { name: 'John' })
-    t.alike(john, { name: 'John', gender: Male, birth: null })
+    t.alike(john, { name: 'John', gender: NotSpecified, birth: null })
   }
 
   await db.insert('@db/members', {
     name: 'John',
-    gender: Male,
+    gender: NotSpecified,
     birth: {
       year: 1993
     }
@@ -29,7 +29,7 @@ test('update nested', async function ({ create, bee }, t) {
     const john = await db.get('@db/members', { name: 'John' })
     t.alike(john, {
       name: 'John',
-      gender: Male,
+      gender: NotSpecified,
       birth: {
         year: 1993
       }

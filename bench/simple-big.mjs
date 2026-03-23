10c3c117a9d872ff4c4a3fdfa4947c31586d1f69
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-03-23 17:31:43 +0100

    Illustrate bee2 bug

--- bench/simple-big.mjs
+++ bench/simple-big.mjs
@@ -15,7 +15,7 @@ console.timeEnd('boot')
 
 let i = oldest ? oldest.age + 1 : 0
 
-while (i < 10_000_000) {
+while (i < 300_000) {
   console.time('inserting')
   const all = []
   for (let j = 0; j < 25_000; j++) {
@@ -29,6 +29,8 @@ while (i < 10_000_000) {
   console.timeEnd('flushing')
   console.log('total:', i)
 }
+
+console.log('queries...')
 
 await timeQuery('10k members-by-age', '@db/members-by-age', { gt: { age: 90_000 }, limit: 10_000 })
 await timeQuery('10 members-by-age', '@db/members-by-age', { limit: 10 })

cbd0c2e684e793dea6c44e59108d767f2c92c160
Christophe Diederichs <chm-diederichs@protonmail.com>
2026-03-30 15:50:30 +0100

    add test for empty head checkout

--- test/checkout.js
+++ test/checkout.js
@@ -45,3 +45,17 @@ test.bee2('basic checkouts', async function ({ create }, t) {
 
   await db.close()
 })
+
+test.bee2('empty checkout', async function ({ create }, t) {
+  const db = await create()
+
+  await db.insert('@db/members', { id: 'someone', age: 40 })
+  await db.flush()
+
+  const checkout = { key: null, length: 0 }
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'someone', age: 40 }])
+  t.alike(await db.find('@db/members', { checkout }).toArray(), [])
+
+  await db.close()
+})

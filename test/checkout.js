ced26260fb26acf7f780c9d8de75bd33b10a7404
Dominic Cassidy <dominic@cassidy.casa>
2026-03-30 18:44:46 +0100

    bee2 index stream (#94)
    * Add support for bee2 checkout in IndexStream
    
    * bee2 checkout in getValue
    
    * close snapshot if we own it
    
    * only checkout if needed
    
    * complete checkout test
    
    * lint
    
    * review by @mafintosh
    
    * add test for empty head checkout
    
    * avoid feature sniffing
    
    * lint
    
    ---------
    
    Co-authored-by: Christophe Diederichs <chm-diederichs@protonmail.com>
    Co-authored-by: Mathias Buus <mathiasbuus@gmail.com>

--- test/checkout.js
+++ test/checkout.js
@@ -22,3 +22,40 @@ test.bee('basic checkouts', async function ({ create }, t) {
 
   await db.close()
 })
+
+test.bee2('basic checkouts', async function ({ create }, t) {
+  const db = await create()
+
+  await db.insert('@db/members', { id: 'someone', age: 40 })
+  await db.flush()
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'someone', age: 40 }])
+
+  const checkout = db.engine.head()
+
+  await db.insert('@db/members', { id: 'someone', age: 41 })
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'someone', age: 41 }])
+  t.alike(await db.find('@db/members', { checkout }).toArray(), [{ id: 'someone', age: 40 }])
+
+  await db.flush()
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'someone', age: 41 }])
+  t.alike(await db.find('@db/members', { checkout }).toArray(), [{ id: 'someone', age: 40 }])
+
+  await db.close()
+})
+
+test.bee2('empty checkout', async function ({ create }, t) {
+  const db = await create()
+
+  await db.insert('@db/members', { id: 'someone', age: 40 })
+  await db.flush()
+
+  const checkout = { key: null, length: 0 }
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'someone', age: 40 }])
+  t.alike(await db.find('@db/members', { checkout }).toArray(), [])
+
+  await db.close()
+})
